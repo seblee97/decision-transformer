3eb5a1fd6519cdc05f8f3c71d127de319f7f9c91
Sebastian Lee <slee1@rusty1.cm.cluster>
2025-04-30 14:34:57 -0400

    checkpoint path

--- atari/run_dt_atari.py
+++ atari/run_dt_atari.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import logging
 # make deterministic
 from mingpt.utils import set_seed
@@ -19,6 +20,14 @@ import blosc
 import argparse
 from create_dataset import create_dataset
 
+import time 
+import datetime
+
+raw_datetime = datetime.datetime.fromtimestamp(time.time())
+exp_timestamp = raw_datetime.strftime("%Y-%m-%d-%H-%M-%S")
+exp_path = os.path.join("results", exp_timestamp)
+os.makedirs(exp_path, exist_ok=True)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', type=int, default=123)
 parser.add_argument('--context_length', type=int, default=30)
@@ -30,7 +39,7 @@ parser.add_argument('--game', type=str, default='Breakout')
 parser.add_argument('--batch_size', type=int, default=128)
 # 
 parser.add_argument('--trajectories_per_buffer', type=int, default=10, help='Number of trajectories to sample from each of the buffers.')
-parser.add_argument('--data_dir_prefix', type=str, default='./dqn_replay/')
+parser.add_argument('--data_dir_prefix', type=str, default='/mnt/home/slee1/decision-transformer/dataset/')
 args = parser.parse_args()
 
 set_seed(args.seed)
@@ -69,9 +78,11 @@ obss, actions, returns, done_idxs, rtgs, timesteps = create_dataset(args.num_buf
 
 # set up logging
 logging.basicConfig(
+        filename=os.path.join(exp_path, "log.log"),
         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
         level=logging.INFO,
+        force=True
 )
 
 train_dataset = StateActionReturnDataset(obss, args.context_length*3, actions, done_idxs, rtgs, timesteps)
@@ -81,10 +92,16 @@ mconf = GPTConfig(train_dataset.vocab_size, train_dataset.block_size,
 model = GPT(mconf)
 
 # initialize a trainer instance and kick off training
+if args.game == 'KeyDoorEnv':
+    kd_env_path = os.path.join(args.data_dir_prefix, 'KeyDoorEnv', 'kd_env_config')
+else:
+    kd_env_path = None
+
 epochs = args.epochs
 tconf = TrainerConfig(max_epochs=epochs, batch_size=args.batch_size, learning_rate=6e-4,
                       lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(train_dataset)*args.context_length*3,
-                      num_workers=4, seed=args.seed, model_type=args.model_type, game=args.game, max_timestep=max(timesteps))
+                      num_workers=4, seed=args.seed, model_type=args.model_type, game=args.game, max_timestep=max(timesteps),
+                      ckpt_path=os.path.join(exp_path), kd_env_path=kd_env_path)
 trainer = Trainer(model, train_dataset, None, tconf)
 
 trainer.train()
